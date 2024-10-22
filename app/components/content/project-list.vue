8c48755e0a46a4015c6f208ef775db8bea7c2b1a
Alejandro Guevara <alejandroguevara@Alejandros-MacBook-Pro.local>
2024-10-22 16:25:10 +0200

    add no prose use into the project list component

--- app/components/content/project-list.vue
+++ app/components/content/project-list.vue
@@ -1,31 +1,33 @@
 <template>
-  <section v-if="pending">
-    <div>Loading...</div>
-  </section>
-  <section v-else-if="error">
-    <div>Error: {{ error.message }}</div>
-  </section>
-  <section v-else>
-    <ul class="grid grid-cols-1 gap-4">
-      <li
-        v-for="repository in repos"
-        :key="repository.id"
-        class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
-      >
-        <a :href="repository.html_url" target="_blank">
-          <div class="flex items-center justify-between">
-            <div class="text-xl font-semibold">{{ repository.name }}</div>
-            <div class="text-sm">{{ repository.stargazers_count }} *</div>
-          </div>
-          <p class="text-sm">{{ repository.description }}</p>
-        </a>
-      </li>
-    </ul>
-  </section>
+  <div class="not-prose font-mono">
+    <section v-if="pending">
+      <div>Loading...</div>
+    </section>
+    <section v-else-if="error">
+      <div>Error: {{ error.message }}</div>
+    </section>
+    <section v-else>
+      <ul class="grid grid-cols-1 gap-4">
+        <li
+          v-for="repository in repos"
+          :key="repository.id"
+          class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+        >
+          <a :href="repository.html_url" target="_blank">
+            <div class="flex items-center justify-between">
+              <div class="text-xl font-semibold">{{ repository.name }}</div>
+              <div class="text-sm">{{ repository.stargazers_count }} *</div>
+            </div>
+            <p class="text-sm">{{ repository.description }}</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { useRuntimeConfig } from '#app';
+import { useRuntimeConfig } from "#app";
 const config = useRuntimeConfig();
 const { error, pending, data } = await useFetch(config.public.githubRepoUrl);
 const repos = computed(() =>
